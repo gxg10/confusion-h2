20912a90a79750574436e93fc14ffac167c6e8ee
Brande George <george.brande@gmail.com>
2018-08-12 22:41:44 +0300

    assignment 3

---
+++ authenticate.js
@@ -0,0 +1,54 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config.js');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function(user) {
+    return jwt.sign(user, config.secretKey,
+        {expiresIn: 3600});
+};
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts,
+    (jwt_payload, done) => {
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            else if (user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req,res,next) {
+	if (req.user.admin === true) {
+		console.log("hahahha ADMIN");
+		res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        next()
+	}
+	else {
+		console.log("NU E ADMIN");
+		err = new Error('Nu este voie');
+    	err.status = 404;
+    	return next(err);
+	}
+};
---
+++ routes/leadersRouter.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
+
+const Leaders = require('../models/leaders');
+
+const leadersRouter = express.Router();
+
+leadersRouter.use(bodyParser.json());
+
+leadersRouter.route('/')
+.get((req,res,next) => {
+	Leaders.find({})
+	.then((leaders)=>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(leaders);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+})
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+	Leaders.create(req.body)
+	.then((leader) => {
+		console.log('Leader created ', leader);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+	res.statusCode = 403;
+	res.end('put operation not supported on /leaders');
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+	Leaders.remove({})
+	.then((resp)=>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type','application/json');
+		res.json(resp);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+});
+
+leadersRouter.route('/:leaderId')
+.get((req,res,next) => {
+	Leaders.findById(req.params.leaderId)
+	.then((leader) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type','application/json');
+		res.json(leader);
+	}, (err) =>next(err))
+	.catch((err) => next(err));
+})
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+	res.statusCode = 403;
+	res.end('post operation not supported on /leaders/'
+		+ req.params.leaderId);
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+	Leaders.findByIdAndUpdate(req.params.leaderId, {
+		$set: req.body
+	}, {new: true})
+	.then((leader)=>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type','application/json');
+		res.json(leader);
+	},(err) => next(err))
+	.catch((err)=> next(err));
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+	Leaders.findByIdAndRemove(req.params.leaderId)
+	.then((resp)=>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type','application/json');
+		res.json(resp);
+	},(err)=> next(err))
+	.catch((err) => next(err));
+});
+
+module.exports = leadersRouter;
